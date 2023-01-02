8ca0cae78bd10dc582fa454028ca9a1e127154e3
Alex Merced <alex@alexmerced.dev>
2023-01-02 10:43:30 -0500

    improved typedefs

--- types/types.d.ts
+++ types/types.d.ts
@@ -1,22 +1,22 @@
-interface UnknownObjectShape {
+export interface UnknownObjectShape {
   [key: string]: any;
 }
 
-interface ActionArg {
+export interface ActionArg {
   type: string;
   payload: UnknownObjectShape;
 }
 
-interface SimpleRPCResponse {
+export interface SimpleRPCResponse {
   result: any;
 }
 
-interface SimpleRPCClientConfig {
+export interface SimpleRPCClientConfig {
   url: string;
   headers: UnknownObjectShape;
 }
 
-type SimpleRPCClient = (action: ActionArg) => Promise<SimpleRPCResponse>;
+export type SimpleRPCClient = (action: ActionArg) => Promise<SimpleRPCResponse>;
 
 declare function createClient(config: SimpleRPCClientConfig): SimpleRPCClient;
 
